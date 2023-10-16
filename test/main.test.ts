b5927b3def90d4651f39a9d9d64f1900be850e1a
Dennis <10233439+idea404@users.noreply.github.com>
2023-10-16 10:56:28 +0200

    chore: use simpler provider syntax

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,7 +2,6 @@ import { expect } from 'chai';
 import { Wallet, Provider, Contract } from 'zksync-web3';
 import * as hre from 'hardhat';
 import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
-import { ZkSyncProviderAdapter } from '@matterlabs/hardhat-zksync-node';
 
 const RICH_WALLET_PK =
   '0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110';
@@ -14,7 +13,7 @@ async function deployGreeter(deployer: Deployer): Promise<Contract> {
 
 describe('Greeter', function () {
   it("Should return the new greeting once it's changed", async function () {
-    const provider = (hre.network.provider as ZkSyncProviderAdapter)._zkSyncProvider
+    const provider = new Provider(hre.network.config.url);
 
     const wallet = new Wallet(RICH_WALLET_PK, provider);
     const deployer = new Deployer(hre, wallet);
